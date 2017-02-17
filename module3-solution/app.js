2012f43efb55919d3c0f815318e6a7e6a54c09fb
Stefano Vollono <stefanovollono@gmail.com>
2017-02-17 18:24:17 +0100

    fix

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -9,6 +9,7 @@
 *
 */
 (function(){
+  'use strict';
 
   angular
     .module('NarrowItDownApp', [])
@@ -23,16 +24,27 @@
 
   // Declare and create a NarrowItDownController (with controller as syntax) that will wrap your search textbox and button as well as the list of found items.
   function NarrowItDownController ($scope, MenuSearchService) {
-    // The controller should call the getMatchedMenuItems method when appropriate and store the result in a property called "found" attached to the controller instance.
-    var promise = MenuSearchService.getMatchedMenuItems();
+    var local = this;
 
-    promise
-      .then(function (result) {
-        console.log(result.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+    // In the NarrowItDownController, simply remove that item from the found array.
+    local.removeItem = function (index) {
+      // You can do that using the Array's splice() method. For example, to remove an item with the index of 3 from the found array, you would call found.splice(3, 1);.
+      found.splice(index, 1);
+    }
+
+    // The controller should call the getMatchedMenuItems method when appropriate and store the result in a property called "found" attached to the controller instance.
+    local.showListMenu = function () {
+      var promise = MenuSearchService.getMatchedMenuItems(local.searchTerm);
+    
+      promise
+        .then(function (result) {
+          local.found = result.data.menu_items;
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+
 
   }
 
@@ -44,20 +56,14 @@
     service.getMatchedMenuItems = function (searchTerm) {
       var response = $http({
         method: 'GET',
-        url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
-        params: {
-          search: 'as'
-        }
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
       });
 
+      // Once it gets all the menu items, it should loop through them to pick out the ones whose description matches the searchTerm.
+
+      // Once a list of found items is compiled, it should return that list (wrapped in a promise).
       return response;
     };
-
-    // That method will be responsible for reaching out to the server (using the $http service) to retrieve the list of all the menu items.
-    // Once it gets all the menu items, it should loop through them to pick out the ones whose description matches the searchTerm.
-    // Once a list of found items is compiled, it should return that list (wrapped in a promise).
-    // The URL for the REST Endpoint is https://davids-restaurant.herokuapp.com/menu_items.json.
-
 
   }
 
@@ -65,6 +71,9 @@
   // Declare and create foundItems directive.
   function FoundItems () {
     var ddo = {
+      restrict: 'E',
+      templateUrl: 'foundItems.html',
+      replace: true,
 
       // The list should be displayed using this directive which takes the found array of items specified on it as an attribute (think one-way binding with '<').
       // To implement the functionality of the "Don't want this one!" button, the directive should also provide an on-remove attribute that will use function reference binding to invoke the parent controller removal an item from the found array based on an index into the found array.
@@ -76,11 +85,6 @@
 
     return ddo;
   }
-
-
-  // The index should be passed in from the directive to the controller. (Note that we implemented almost identical type of behavior in the Lecture 30 Part 2
-  // In the NarrowItDownController, simply remove that item from the found array.
-  // You can do that using the Array's splice() method. For example, to remove an item with the index of 3 from the found array, you would call found.splice(3, 1);.
 
 }());
 
